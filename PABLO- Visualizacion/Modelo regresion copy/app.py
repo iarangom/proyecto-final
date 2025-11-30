f4f1b6c23f359185e0fec5abd17fb1a8644a6f8f
Isabella Arango <i.arangom@uniandes.edu.co>
2025-11-30 16:28:46 -0500

    poner funciones de modelos en app de pablo

--- PABLO- Visualizacion/Modelo regresion copy/app.py
+++ PABLO- Visualizacion/Modelo regresion copy/app.py
@@ -1,8 +1,8 @@
 #APP DE PABLO 
-
+import pandas as pd
 
 from modelo_reg_funcion import predecir_precio #Esto importa la funcion creada en el archivo modelo reg. Para predecir el precio solo tienes que llamar la funcion 
-
+from modelo_reg_funcion import predecir_recomendable
 '''EJEMPLO DEL PARAMETRO ESPERADO POR predecir_precio()
 df_entrada = pd.DataFrame([{
     "bathroomsf":2 ,                  # Numero de baños (puede ser decimal)
@@ -27,3 +27,39 @@ conda env create -f conda.yaml -n modelo_regresion
 # 3. Activar el entorno
 conda activate modelo_regresion
 '''
+
+
+
+#EJEMPLO DE DF QUE RECIBE CADA FUNCION #BORRALOS DESPUES
+
+#DF para predecir precios
+df_entrada1 = pd.DataFrame([{
+    "bathroomsf":2 ,                  # Numero de baños (puede ser decimal)
+    'bedrooms':4,                      # Numero de cuartos (puede ser decimal)
+    'room_type':1,                      # Binario 'Entire home/apt': 1, 'Private room': 0
+    'review_scores_location':4.6 ,      # Decimal [0,5]
+    'has_jacuzzi':1 ,                   # Binario : 1 si si tiene, 0 si no
+    'has_tv_cable':1 ,                  # Binario : 1 si si tiene, 0 si no
+    'has_pool':0 ,                      # Binario : 1 si si tiene, 0 si no
+    'estimated_occupancy_l365d': 234 ,  #Numero entero [0,365]
+    'review_scores_rating': 4.6 ,       # Decimal [0,5]
+    'accommodates': 4 ,                 # Entero numero de personas que pueden dormir ahi
+    }])
+#DF para predecir recomendable o no 
+df_entrada2 = pd.DataFrame([{
+    'host_response_rate': 0.05,     # decimal entre 0 y 1
+    'host_is_superhost': 0,         # 1 si es superhost, 0 si no
+    'host_identity_verified': 1,    # 1 si verificó identidad, 0 si no
+    'instant_bookable': 0,          # 1 si se puede reservar instantáneamente
+    'minimum_nights': 2,            # noches mínimas
+    'maximum_nights': 4,           # noches máximas
+    'bathroomsf': 1.5,              # cantidad de baños (decimal o entero)
+    'beds': 2,                      # número de camas
+    'has_wifi': 1,                  # amenity (1/0)
+    'has_ac_heating': 0,            # amenity (1/0)
+    'has_self_checkin': 0,          # amenity (1/0)
+    'has_tv_cable': 0               # amenity (1/0)
+}])
+
+print(predecir_precio(df_entrada1)) #EJEMPLO DE COMO LLAMAR A LAS FUNCIONES BORRAR
+print(predecir_recomendable(df_entrada2)) #EJEMPLO DE COMO LLAMAR A LAS FUNCIONES BORRAR
--- PABLO- Visualizacion/Modelo regresion copy/modelo_reg_funcion.py
+++ PABLO- Visualizacion/Modelo regresion copy/modelo_reg_funcion.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 # Solo 1 vez es necesario 
 preprocesamiento = joblib.load("preprocesamiento.pkl")
+preprocesamiento_class = joblib.load("preprocesamiento_class.pkl")
 modelo_reg = joblib.load("model_reg.pkl")
 modelo_clas = joblib.load("model_clas.pkl")
 
@@ -17,12 +18,13 @@ def predecir_precio(datos_dict):
     datos_dict: diccionario con las 11 keys de FEATURES.
     Devuelve el precio predicho (float).
     """
-    df_entrada = pd.DataFrame([datos_dict], columns=FEATURES)
-    X_nuevo = preprocesamiento.transform(df_entrada)
-    pred_log = model_reg.predict(X_nuevo)
+    X_nuevo = preprocesamiento.transform(datos_dict)
+    pred_log = modelo_reg.predict(X_nuevo)
     return float(np.exp(pred_log)[0])
 
 #EJEMPLO DE DF QUE USAR LA FUNCION predecir_precio
+
+
 df_entrada = pd.DataFrame([{
     "bathroomsf":2 ,                  # Numero de baños (puede ser decimal)
     'bedrooms':4,                      # Numero de cuartos (puede ser decimal)
@@ -37,21 +39,21 @@ df_entrada = pd.DataFrame([{
     }])
 
 df_entrada = pd.DataFrame([{
-    'host_response_rate': 0.95,     # decimal entre 0 y 1
-    'host_is_superhost': 1,         # 1 si es superhost, 0 si no
+    'host_response_rate': 0.05,     # decimal entre 0 y 1
+    'host_is_superhost': 0,         # 1 si es superhost, 0 si no
     'host_identity_verified': 1,    # 1 si verificó identidad, 0 si no
-    'instant_bookable': 1,          # 1 si se puede reservar instantáneamente
+    'instant_bookable': 0,          # 1 si se puede reservar instantáneamente
     'minimum_nights': 2,            # noches mínimas
-    'maximum_nights': 30,           # noches máximas
+    'maximum_nights': 4,           # noches máximas
     'bathroomsf': 1.5,              # cantidad de baños (decimal o entero)
     'beds': 2,                      # número de camas
     'has_wifi': 1,                  # amenity (1/0)
     'has_ac_heating': 0,            # amenity (1/0)
-    'has_self_checkin': 1,          # amenity (1/0)
-    'has_tv_cable': 1               # amenity (1/0)
+    'has_self_checkin': 0,          # amenity (1/0)
+    'has_tv_cable': 0               # amenity (1/0)
 }])
 
-def predecir_recomendable(df_entrada, threshold=0.5):
+def predecir_recomendable(df_entrada):
     """
     df_entrada: DataFrame con 1 fila y las columnas empleadas en el modelo.
     threshold: valor de corte para clasificar (default 0.5).
@@ -59,9 +61,9 @@ def predecir_recomendable(df_entrada, threshold=0.5):
     Returns:
         recomendacion (str): "Recomendable" o "No recomendable"
     """
-
+    threshold=0.5
     # Preprocesar
-    X_nuevo = preprocesamiento.transform(df_entrada)
+    X_nuevo = preprocesamiento_class.transform(df_entrada)
     # Probabilidad predicha de clase 1 (recomendable)
     prob = modelo_clas.predict_proba(X_nuevo)[0, 1]
     # Predicción textual
@@ -70,4 +72,3 @@ def predecir_recomendable(df_entrada, threshold=0.5):
     else:
         return "No recomendable"
     
-print(predecir_recomendable(df_entrada))
